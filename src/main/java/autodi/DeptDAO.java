9a763d7586bfbb0834e9556df02011db3d2f5708
buxy <bxy_xiaoyu@163.com>
2019-09-07 18:13:19 +0800

    this 77777777

--- src/main/java/autodi/DeptDAO.java
+++ src/main/java/autodi/DeptDAO.java
@@ -3,4 +3,8 @@ package autodi;
 public interface DeptDAO {
     void delete();
     //    //ashdlkasjgfhjas
+    //哈三联可见光发军绿色
+    //sahfkahofuhakjfhakljfhka
+    //kajfhklajsfa
+    //12111111111111
 }
